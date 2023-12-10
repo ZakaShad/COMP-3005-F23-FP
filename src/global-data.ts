857033989e84fb91349205dae4024f701ffbb4b1
Shady Zakaria <shadyashraf2002@gmail.com>
2023-12-09 19:10:01 -0500

    ...

--- src/global-data.ts
+++ src/global-data.ts
@@ -1,14 +1,11 @@
 import { Pool } from 'pg';
 
 const DB = new Pool({
-    user: 'postgres', //postgres by default
-    // user: process.env.DB_USER, //postgres by default
-    // password: process.env.DB_PASS,
-    password: 'mario200',
+    user: process.env.DB_USER, //postgres 
+    password: process.env.DB_PASS,
     host: 'localhost',
     port: 5432, // default Postgres port
-    // database: process.env.DB_NAME //A4 for me
-    database: 'FP' //A4 for me
+    database: process.env.DB_NAME //FP 
 });
 
 export default DB;
