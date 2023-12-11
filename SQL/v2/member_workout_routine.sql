0e7c71adb2e51ec15faa5db3fc07b14fe59f74fe
Shady Zakaria <shadyashraf2002@gmail.com>
2023-12-11 04:57:46 -0500

    added a bunch of stuff

---
+++ SQL/v2/member_workout_routine.sql
@@ -0,0 +1,5 @@
+INSERT INTO member_workout_routine
+VALUES
+('ratilious002@gmail.com', 3),
+('ratilious002@gmail.com', 4),
+('ratilious002@gmail.com', 5);
--- src/routes/member.ts
+++ src/routes/member.ts
@@ -5,11 +5,11 @@ const routerMem = express.Router();
 
 routerMem.get("/:username", async (req,res) => { 
     const memberQuery = `SELECT * FROM Member WHERE email = '${req.params.username}' `
-    // const PRsQuery = `SELECT workout_name, weight, reps FROM goal_pr WHERE member_username='${req.params.username}' `
     
     const routineQuery = `
-    SELECT name FROM member_workout_routine NATURAL JOIN workout_routine
-    WHERE member='${req.params.username}';
+    SELECT * FROM member_workout_routine, workout_routine
+    WHERE member='${req.params.username}'
+    AND routine=routine_id;
     `
 
     try{
@@ -41,5 +41,79 @@ routerMem.get("/:username/goal", async (req,res) => {
 
 });
 
+routerMem.get("/:username/editProfile", async (req,res) => { 
+    const memberQuery = `SELECT * FROM Member WHERE email = '${req.params.username}' `
+    try{
+        const result = await DB.query(memberQuery);
+        const memberData = result.rows[0];
+        res.render('updateMember', memberData);
+    }catch(err){
+        console.log(err);
+        res.send("internal server error");
+    }
+
+});
+
+// routerMem.post("/toPut", (req, res) => {
+
+// })
+
+// This is really bad design as post should be used for adding to DB
+routerMem.post("/edit", async(req, res) => {
+    console.log("ASDFASDFASDF");
+    const email = req.oidc.user.email;
+    const fname = req.body.firstName;
+    const lname = req.body.lastName;
+    const dob = req.body.dob;
+    const {height, weight, RHR, MHR, desiredWeight} = req.body;
+    
+    const usersQuery = `
+    UPDATE User
+    SET first_name='${fname}', last_name='${lname}', birthday='${dob}'
+    INSERT INTO Users (email, first_name, last_name, registration_date, birthday, type);
+    `
+
+    const subQuery = `
+    UPDATE Member 
+    SET desired_weight=${desiredWeight}, height=${height}, weight=${weight}, rhr=${RHR}, mh=${MHR}
+    WHERE email='${email}';
+    `
+
+    try{
+        const query = usersQuery + subQuery;
+        await DB.query(query);
+    } catch(err){
+        console.log(err);
+        res.send("Internal server error");
+    }
+
+    res.redirect("/");
+});
+
+routerMem.get("/:email/schedule", async(req, res) => {
+    const query = `
+    SELECT * FROM event, member_event_registration AS mer
+    WHERE event.event_id = mer.event
+    AND
+    mer.member = '${req.params.email}'
+    `
+
+    const sessionQuery = `
+    SELECT * FROM training_session
+    WHERE member = '${req.params.email}'
+    `
+
+    try{
+        const eventResults = await DB.query(query);
+        const events = eventResults.rows;
+
+        const sessionRes = await DB.query(sessionQuery);
+        const sessions = sessionRes.rows;
+        res.render('memberSched', {events, sessions});
+    }catch(err){
+        console.log(err);
+        res.send("internal server error");
+    }
+});
 
 export default routerMem;
